716376ef4e4ddbb341c3b17a095f518dd8f5b91e
Mohammadreza Asgari <mr.asgari8015@gmail.com>
2022-03-03 10:43:34 +0330

    Added new commands with yargs package

--- app.js
+++ app.js
@@ -1,2 +1,39 @@
-const chalk = require("chalk");
+const yargs = require('yargs');
 
+//Adding "add" command
+yargs.command({
+    command: 'add',
+    describe: 'adding a new note',
+    handler: function () {
+        console.log('added a note')
+    }
+})
+
+//Adding "remove" command
+yargs.command({
+    command: 'remove',
+    describe: 'removing a note',
+    handler: function () {
+        console.log('removed a note')
+    }
+})
+
+//Adding "list" command
+yargs.command({
+    command: 'list',
+    describe: 'listing notes',
+    handler: function () {
+        console.log('listed notes')
+    }
+})
+
+//Adding "read" command
+yargs.command({
+    command: 'read',
+    describe: 'reading notes',
+    handler: function () {
+        console.log('read notes')
+    }
+})
+
+console.log(yargs.argv)
